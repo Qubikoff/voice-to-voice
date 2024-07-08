2b2589ed5c47f6caa04065940d91416df18e29a8
Sergey Sharuev <sharueff_samara@mail.ru>
2024-07-09 00:41:53 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,23 @@ from recognizer import compare_models
 from utils import respond_to_text, text_to_speech, record_audio
 import os
 
-if __name__ == "__main__":
+def main():
     filename = "input.wav"
-    record_audio(filename)
-    recognized_text = compare_models(filename)
-    response_text = respond_to_text(recognized_text)
-    text_to_speech(response_text)
-    print(f"Распознанный текст: {recognized_text}")
-    print(f"Ответ: {response_text}")
-    os.remove(filename)
+    while True:
+        record_audio(filename)
+        recognized_texts = compare_models(filename)
+        
+        for recognized_text in recognized_texts:
+            response_text = respond_to_text(recognized_text)
+            text_to_speech(response_text)
+            print(f"Распознанный текст: {recognized_text}")
+            print(f"Ответ: {response_text}")
+        
+        os.remove(filename)
+        
+        repeat = input("Хотите попробовать снова? (да/нет): ").strip().lower()
+        if repeat != "да":
+            break
+
+if __name__ == "__main__":
+    main()
